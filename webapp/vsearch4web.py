fb39f836ddc083bdb3126baddfd5f1b26432cd09
Artem <arkulikov@yandex.ru>
2021-09-26 12:40:35 +0300

    changed files

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from my_vsearch import vsearch as vs
 
 app = Flask(__name__)
 
 
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results: '
+    results = str(vs.search4letters(phrase, letters))
+    return render_template('result.html', the_phrase=phrase, the_letters=letters, the_title=title,
+                           the_results=results,)
+
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello from Flask!'
-
-
-@app.route('/search4')
-def do_search() -> str:
-    return str(vs.search4letters('life, the universe, and everything!', 'eiru,!'))
-
-
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 
-app.run()
+app.run(debug=True)
